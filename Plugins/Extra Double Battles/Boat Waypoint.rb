b7e71cbe1406581ade5edd94112b6ae4bd62dccc
AlphaKretin <deewhydeeecks@gmail.com>
2024-09-21 16:03:58 +0930

    Rename location
    Going to change my mod name and this scans better for that

--- Plugins/Extra Double Battles/Boat Waypoint.rb
+++ Plugins/Extra Double Battles/Boat Waypoint.rb
@@ -1,5 +1,5 @@
-DOCK_LOCATIONS[:ILLUSORY_PARADISE] = {
-  :map_name => "Illusory Paradise",
+DOCK_LOCATIONS[:BATTLE_PARADISE] = {
+  :map_name => "Battle Paradise",
   :map_id => 931,
   :event_id => 8,
 }
